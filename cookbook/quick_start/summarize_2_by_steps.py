065e7ebbce43e1aea1e5df0c7d0fc6723ad78316
Louis Choquel <louis@pipelex.com>
2025-05-19 23:53:09 +0200

    Added summarize_by_steps which generates the history flow chart

---
+++ cookbook/quick_start/summarize_2_by_steps.py
@@ -0,0 +1,42 @@
+# SPDX-FileCopyrightText: © 2025 Evotis S.A.S.
+# SPDX-License-Identifier: Elastic-2.0
+# "Pipelex" is a trademark of Evotis S.A.S.
+
+import asyncio
+
+from pipelex import pretty_print
+from pipelex.core.working_memory_factory import WorkingMemoryFactory
+from pipelex.hub import get_report_delegate
+from pipelex.mission.mission_tracker import job_history
+from pipelex.pipelex import Pipelex
+from pipelex.run import run_pipe_code
+
+from pipelex_libraries.pipelines.quick_start.summarize import StructuredSummary
+
+
+async def summarize_by_steps(text: str):
+    job_history.activate()
+    # Load the working memory with the text
+    working_memory = WorkingMemoryFactory.make_from_text(text=text)
+
+    # Run the pipe
+    pipe_output = await run_pipe_code(
+        pipe_code="summarize_by_steps",
+        working_memory=working_memory,
+    )
+
+    summary_text = pipe_output.main_stuff_as_text
+
+    # Print the output
+    pretty_print(summary_text, title="Summarized by steps")
+
+    # Get the report (tokens used and cost)
+    get_report_delegate().general_report()
+    job_history.print_mermaid_flowchart_url()
+
+
+with open("data/sample_text_1.txt", "r") as f:
+    text = f.read()
+
+Pipelex.make()
+asyncio.run(summarize_by_steps(text))
