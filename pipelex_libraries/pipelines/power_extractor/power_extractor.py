2f52056cbbebe47cbdf226786097ad8260753246
antoinepipelex <antoine@pipelex.com>
2025-05-20 08:28:30 +0200

    Add licenses on top of files

--- pipelex_libraries/pipelines/power_extractor/power_extractor.py
+++ pipelex_libraries/pipelines/power_extractor/power_extractor.py
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: © 2025 Evotis S.A.S.
+# SPDX-License-Identifier: Elastic-2.0
+# "Pipelex" is a trademark of Evotis S.A.S.
+
 from typing import List
 
 from pipelex.core.stuff_content import ImageContent, PageContent, TextAndImagesContent, TextContent
