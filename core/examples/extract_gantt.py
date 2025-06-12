f86ebfc46f4f95b4254744958b886c7476069f75
thomashebrard <thomas@pipelex.com>
2025-06-12 16:31:57 +0200

    make cc

--- core/examples/extract_gantt.py
+++ core/examples/extract_gantt.py
@@ -6,7 +6,6 @@ from pipelex.pipelex import Pipelex
 from pipelex.pipeline.execute import execute_pipeline
 
 from pipelex_libraries.pipelines.examples.extraction.gantt import GanttChart
-from utils.results_utils import output_result
 
 SAMPLE_NAME = "extract_gantt"
 IMAGE_URL = "assets/gantt/gantt_tree_house.png"
