be7f22eaffa4a644a1745368978ed31a87552699
Louis Choquel <louis@pipelex.com>
2025-06-14 00:20:25 +0200

    test examples

--- tests/e2e/test_examples.py
+++ tests/e2e/test_examples.py
@@ -1,3 +1,21 @@
 class TestExamples:
+    def test_extract_dpe(self):
+        import examples.extract_dpe
+
+    def test_extract_gantt(self):
+        import examples.extract_gantt
+
+    def test_extract_generic(self):
+        import examples.extract_generic
+
+    def test_extract_proof_of_purchase(self):
+        import examples.extract_proof_of_purchase
+
+    def test_extract_table(self):
+        import examples.extract_table
+
+    def test_invoice_extractor(self):
+        import examples.invoice_extractor
+
     def test_simple_ocr(self):
         import examples.simple_ocr
