a8d184c6b046e9d69313c74ccdf5f86c3ed1bac5
Louis Choquel <louis@pipelex.com>
2025-06-16 10:07:13 +0200

    Mark test_write_screenplay() as xfail

--- tests/e2e/test_wip.py
+++ tests/e2e/test_wip.py
@@ -1,4 +1,5 @@
 import pytest
+from pipelex.exceptions import PipeInputError
 
 
 @pytest.mark.dry_runnable
@@ -6,5 +7,6 @@ class TestWip:
     def test_write_tweet(self):
         import examples.wip.write_tweet  # noqa: F401
 
+    @pytest.mark.xfail(raises=PipeInputError, reason="Requires new upcoming features to complete the pipeline")
     def test_write_screenplay(self):
         import examples.wip.write_screenplay  # noqa: F401
