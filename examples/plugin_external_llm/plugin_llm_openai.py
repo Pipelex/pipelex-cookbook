13e5b8e98d4447cc62387f68cd04892212d3b03c
Louis Choquel <louis@pipelex.com>
2025-06-29 12:47:03 +0200

    Cleanup

--- examples/plugin_external_llm/plugin_llm_openai.py
+++ examples/plugin_external_llm/plugin_llm_openai.py
@@ -8,10 +8,6 @@ from pipelex import pretty_print
 from pipelex.cogt.llm.llm_job import LLMJob
 from pipelex.cogt.llm.llm_job_components import LLMJobParams
 from pipelex.cogt.llm.llm_job_factory import LLMJobFactory
-from pipelex.cogt.llm.llm_models.llm_engine import LLMEngine
-from pipelex.cogt.llm.llm_models.llm_family import LLMFamily
-from pipelex.cogt.llm.llm_models.llm_model import LATEST_VERSION_NAME, LLMModel
-from pipelex.cogt.llm.llm_models.llm_platform import LLMPlatform
 from pipelex.cogt.llm.llm_worker_abstract import LLMWorkerAbstract
 from pipelex.cogt.llm.llm_worker_factory import LLMWorkerFactory
 from pipelex.cogt.llm.token_category import NbTokensByCategoryDict, TokenCategory
