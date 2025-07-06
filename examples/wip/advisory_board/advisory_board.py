ceda6cde62d312bcfbe7623b65c81986c2748de2
Louis Choquel <louis@pipelex.com>
2025-07-07 00:42:14 +0200

    Tidy new example

--- examples/wip/advisory_board/advisory_board.py
+++ examples/wip/advisory_board/advisory_board.py
@@ -7,7 +7,7 @@ from pipelex.hub import get_pipeline_tracker, get_report_delegate
 from pipelex.pipelex import Pipelex
 from pipelex.pipeline.execute import execute_pipeline
 
-from pipelex_libraries.pipelines.examples.advisory_board.advisory_orchestrator import StrategicReport
+from pipelex_libraries.pipelines.wip.advisory_board.advisory_orchestrator import StrategicReport
 from utils.results_utils import output_result
 
 SAMPLE_NAME = "advisory_orchestrator"
