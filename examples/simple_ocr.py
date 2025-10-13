defbf3bc29626079f14f94c158bcf108da6ffc66
Louis Choquel <louis@pipelex.com>
2025-10-13 19:07:15 +0200

    Restore features

--- examples/simple_ocr.py
+++ examples/simple_ocr.py
@@ -13,7 +13,7 @@ SAMPLE_NAME = "simple_ocr"
 PDF_URL = "assets/simple_ocr/illustrated_train_article.pdf"
 
 
-async def simple_ocr(pdf_url: str):
+async def simple_ocr(pdf_url: str) -> ListContent[PageContent]:
     pipe_output = await execute_pipeline(
         pipe_code="extract_page_contents_from_pdf",
         input_memory={
@@ -35,4 +35,4 @@ pretty_print(page_content_list)
 output_dir = get_results_dir_path(sample_name=SAMPLE_NAME)
 for page_index, page_content in enumerate(page_content_list.items):
     directory_for_page = f"{output_dir}/page_{page_index}"
-    # page_content.save_to_directory(directory=directory_for_page)
+    page_content.save_to_directory(directory=directory_for_page)
