f9816662a8425091cdc4e3689888a0e4cc6a5f9e
Louis Choquel <louis@pipelex.com>
2025-10-13 19:11:23 +0200

    Fix write_tweet

--- examples/wip/write_tweet/tech_tweet.py
+++
@@ -1,8 +0,0 @@
-from pipelex.core.stuffs.text_content import TextContent
-
-
-class OptimizedTweet(TextContent):
-    """A tweet optimized for Twitter/X engagement following best practices."""
-
-    # TBD
-    pass
--- examples/wip/write_tweet/write_tweet.py
+++ examples/wip/write_tweet/write_tweet.py
@@ -6,13 +6,12 @@ from pipelex.hub import get_pipeline_tracker, get_report_delegate
 from pipelex.pipelex import Pipelex
 from pipelex.pipeline.execute import execute_pipeline
 
-from examples.wip.write_tweet.tech_tweet import OptimizedTweet
 from utils.input_utils import optional_sample_text_from_path
 
 SAMPLE_NAME = "write_tweet"
 
 
-async def optimize_tweet(draft_tweet: str, writing_style: str) -> OptimizedTweet:
+async def optimize_tweet(draft_tweet: str, writing_style: str) -> str:
     pipe_output = await execute_pipeline(
         pipe_code="optimize_tweet_sequence",
         input_memory={
@@ -28,7 +27,7 @@ async def optimize_tweet(draft_tweet: str, writing_style: str) -> OptimizedTweet
     )
 
     # Get the optimized tweet
-    optimized_tweet = pipe_output.main_stuff_as(content_type=OptimizedTweet)
+    optimized_tweet = pipe_output.main_stuff_as_str
     return optimized_tweet
 
 
