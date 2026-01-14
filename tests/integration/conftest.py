0a4757ab76d77cac3bc86c8607e6f5e12d529666
Louis Choquel <8851983+lchoquel@users.noreply.github.com>
2026-01-14 21:31:52 +0100

    Fix running CI without inference and so without service agreement

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,17 +1,21 @@
 import pipelex.config
 import pipelex.pipelex
 import pytest
+from pipelex.test_extras.shared_pytest_plugins import is_inference_disabled_in_pipelex
 from rich import print
 from rich.console import Console
 from rich.traceback import Traceback
 
 
 @pytest.fixture(scope="module", autouse=True)
-def reset_pipelex_config_fixture():
+def reset_pipelex_config_fixture(request: pytest.FixtureRequest):
     # Code to run before each test
     print("\n[magenta]pipelex setup[/magenta]")
     try:
-        pipelex_instance = pipelex.pipelex.Pipelex.make(library_dirs=["examples", "tests/test_pipelines"])
+        pipelex_instance = pipelex.pipelex.Pipelex.make(
+            library_dirs=["examples", "tests/test_pipelines"],
+            disable_inference=is_inference_disabled_in_pipelex(request),
+        )
     except Exception as exc:
         Console().print(Traceback())
         pytest.exit(f"Critical Pipelex setup error: {exc}")
